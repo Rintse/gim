b28d4eab94139c8465c892f6326dd145b944dadd
Lojueusainsusbe <l.dekker.4@umail.leidenuniv.nl>
2020-04-16 19:09:43 +0200

    george h en cc file

---
+++ include/entities/george.h
@@ -0,0 +1,26 @@
+#ifndef george_h
+#define george_h
+
+#include "tools/directions.h"
+
+class Level;
+class Square;
+
+class George {
+public:
+    George();
+    George(Level* l, Square* s);
+
+    void act();
+
+private:
+    Square* curSquare;
+    Level* lvl;
+    int curRound;
+
+    void attackBullets();
+    void attackLasers();
+    void attackTinyGeorges();
+};
+
+#endif
